909761c86fcc63c1b3e2cde8b3940b385e48ffb9
Abdalhaqq Muhammad Saih <abdalhaqqm@gmail.com>
2023-08-05 11:42:38 +0700

    Initial commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -11,7 +12,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Head>
+        <meta charset="utf-8" />
+        <meta http-equiv="x-ua-compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Switch Template</title>
+        <link href="https://fonts.googleapis.com/css?family=Heebo:400,700|IBM+Plex+Sans:600" rel="stylesheet" />
+        <link rel="stylesheet" href="css/style.css" />
+        <script src="https://unpkg.com/scrollreveal@4.0.0/dist/scrollreveal.min.js"></script>
+      </Head>
+      <body className={`${inter.className} is-boxed has-animations`}>{children}</body>
     </html>
   )
 }
